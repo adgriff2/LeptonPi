43462e2aae847f7b5ec3f5c9f83f5b788317fdfe
Adam Griffin <adgriff2@gmail.com>
2014-10-13 21:53:46 -0400

    Initial commit

---
+++ LeptonPi/LeptonPi.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace LeptonPi
+{
+    partial class LeptonPi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "10.4.16.25";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(93, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 240);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LeptonPi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(742, 261);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "LeptonPi";
+            this.Text = "LeptonPi";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ LeptonPi/LeptonPi.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+using System.Threading;
+
+namespace LeptonPi
+{
+    public partial class LeptonPi : Form
+    {
+        public LeptonPi()
+        {
+            InitializeComponent();
+        }
+
+        delegate void updateImageDel(Bitmap bmp);
+
+        private void updateImage(Bitmap bmp)
+        {
+            pictureBox1.BackgroundImage = bmp;
+            pictureBox1.Update();
+            pictureBox1.Refresh();
+        }
+
+        private void getImage()
+        {
+            uint width = 80, height = 60, bytes_per_pixel = 2, bytes_per_header = 4;
+            TcpClient client = new TcpClient();
+            client.Connect(textBox1.Text, 5000);
+            Stream s = client.GetStream();
+            BinaryReader r = new BinaryReader(s);
+            uint VOSPI_PACKET_SIZE = width * bytes_per_pixel + bytes_per_header;
+            byte[] data = new byte[height * VOSPI_PACKET_SIZE];
+            ushort[] grayscale = new ushort[height * width];
+            //byte[] headers = new byte[bytes_per_header * height];
+            uint i, j, max, min;
+            int n = 1;
+            Bitmap bmp = new Bitmap((int)width * 2, (int)height);
+            //pictureBox1.Image = bmp;
+            uint i_width, i_vospi;
+
+            while (n > 0)
+            {
+                n = r.Read(data, 0, data.Length);
+                if (n < data.Length)
+                {
+                    continue;
+                }
+                max = 0;
+                min = 18000;
+                for (i = 0; i < height; i++)
+                {
+                    //for (j = 0; j < bytes_per_header; j++)
+                    //{
+                    //    headers[i * bytes_per_header + j] = data[i * VOSPI_PACKET_SIZE + j];
+                    //}
+                    i_width = i * width;
+                    i_vospi = i * VOSPI_PACKET_SIZE;
+                    for (j = bytes_per_header; j < VOSPI_PACKET_SIZE; j += 2)
+                    {
+                        grayscale[i * width + (j - bytes_per_header) / 2] = (ushort)(data[i_vospi + j] << 8 | data[i_vospi + j + 1]);
+                        if (grayscale[i_width + (j - bytes_per_header) / 2] > max)
+                        {
+                            max = grayscale[i_width + (j - bytes_per_header) / 2];
+                        }
+                        if (grayscale[i_width + (j - bytes_per_header) / 2] < min)
+                        {
+                            min = grayscale[i_width + (j - bytes_per_header) / 2];
+                        }
+                    }
+                }
+                decimal min_max_255 = (decimal)255 / (decimal)(max - min);
+                for (i = 0; i < height; i++)
+                {
+                    i_width = i * width;
+                    for (j = 0; j < width; j++)
+                    {
+                        bmp.SetPixel((int)j, (int)i, Color.FromArgb((int)Math.Round((grayscale[i_width + j] - min) * min_max_255), 0, 0, 0));
+                    }
+                }
+                pictureBox1.Invoke(new updateImageDel(updateImage), bmp);
+            }
+            s.Close();
+            client.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread workerThread = new Thread(getImage);
+            workerThread.Start();
+        }
+    }
+}
